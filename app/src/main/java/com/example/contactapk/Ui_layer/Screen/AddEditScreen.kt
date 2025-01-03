67cd588e914f7dd528f5336a93b33620e0850b8c
SONU <sonuk635812@gmail.com>
2025-01-03 15:23:03 +0530

    this is my

--- app/src/main/java/com/example/contactapk/Ui_layer/Screen/AddEditScreen.kt
+++ app/src/main/java/com/example/contactapk/Ui_layer/Screen/AddEditScreen.kt
@@ -1,9 +1,12 @@
 package com.example.contactapk.Ui_layer.Screen
 
 import android.annotation.SuppressLint
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.R
 
 
 import androidx.compose.foundation.layout.Spacer
@@ -20,13 +23,16 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.contactapk.R
 import com.example.contactapk.Ui_layer.ContactAppViewModel
 import com.example.contactapk.Ui_layer.navigation.HomeScreen
 import java.nio.file.WatchEvent
@@ -38,8 +44,16 @@ fun AddEditScreenUI(
     navController: NavHostController,
     viewModel: ContactAppViewModel = hiltViewModel()
 ) {
-    val imageUrl by remember {
+    var image by remember {
         mutableStateOf(R.drawable.ic_launcher_foreground)
+    }
+
+
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()){
+
+        it?.let {
+            image = it
+        }
     }
     val state = viewModel.state.collectAsState()
     Column(
@@ -47,7 +61,11 @@ fun AddEditScreenUI(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(modifier =)
+        Image(painter = painterResource(id = image), contentDescription = null,
+            modifier =Modifier.clickable {
+
+            })
+        Spacer(modifier = Modifier.height(20.dp))
         OutlinedTextField(
             value = viewModel.state.value.name.value,
             onValueChange = {
@@ -56,7 +74,9 @@ fun AddEditScreenUI(
             placeholder = { Text(text = "Name") },
             label = { Text(text = "Name") },
             shape = RoundedCornerShape(20.dp),
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
         )
         Spacer(modifier = Modifier.height(20.dp))
         OutlinedTextField(
@@ -67,7 +87,9 @@ fun AddEditScreenUI(
             placeholder = { Text(text = "Phone Number") },
             label = { Text(text = "Phone Number") },
             shape = RoundedCornerShape(20.dp),
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
         )
         Spacer(modifier = Modifier.height(20.dp))
         OutlinedTextField(
@@ -78,7 +100,9 @@ fun AddEditScreenUI(
             placeholder = { Text(text = "Email") },
             label = { Text(text = "Email") },
             shape = RoundedCornerShape(20.dp),
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
         )
         Spacer(modifier = Modifier.height(20.dp))
         Button(
